09632cd14b4e4879128404e034a766f28f8d1e3f
Louis Malack <sebago1214@gmail.com>
2023-06-19 18:06:15 +0000

    final

--- controllers/ServeCSS.go
+++ controllers/ServeCSS.go
@@ -2,16 +2,9 @@ package controllers
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func ServeCSS(w http.ResponseWriter, r *http.Request) {
 	filename := r.URL.Path
-	_, err := template.ParseFiles("./template" + filename)
-
-	if err != nil {
-		//w.WriteHeader(404)
-		return
-	}
 	http.ServeFile(w, r, "template"+filename)
 }
